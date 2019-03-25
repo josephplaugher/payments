4ea9bf3e24ffb5dfdf8a6d27bf4bc068b2bddf46
Joseph <joseph@appreciateco.com>
2019-03-25 04:49:08 -0700

    adding login system. need user to be able to sign in

--- src/client/App.js
+++ src/client/App.js
@@ -1,95 +1,97 @@
-import React from 'react'
-import SetUrl from 'Util/SetUrl'
-import ValRules from 'Util/ValRules'
-import EB from 'Util/EB'
-import checkLoginState from 'Util/CheckLoginState'
-import Home from './mainmenu/home'
-import {StripeProvider} from 'react-stripe-elements'
+import React from "react";
+import SetUrl from "Util/SetUrl";
+import ValRules from "Util/ValRules";
+import EB from "Util/EB";
+import checkLoginState from "Util/CheckLoginState";
+import Home from "./mainmenu/Home";
+import { StripeProvider } from "react-stripe-elements";
 
-import 'css/main.css'
-import 'css/userNotify.css'
+import "css/main.css";
+import "css/userNotify.css";
 
 class AppreciateCo extends React.Component {
   constructor(props) {
     super(props);
-    this.stripeKey = ''
+    this.stripeKey = "";
     this.state = {
       error: null,
       isLoggedIn: true,
       userData: {}
-    }
-    this.setLoginState = this.setLoginState.bind(this)
-    this.response = this.response.bind(this)
+    };
+    this.setLoginState = this.setLoginState.bind(this);
+    this.response = this.response.bind(this);
     //this.setLoginState();
-    this.setStripeKey = this.setStripeKey.bind(this)
-    this.setStripeKey()
+    this.setStripeKey = this.setStripeKey.bind(this);
+    this.setStripeKey();
   }
 
   setLoginState = () => {
-    const AppCoToken = sessionStorage.getItem('AppCoToken');
+    const AppCoToken = sessionStorage.getItem("AppCoToken");
     //console.log('appco token', AppCoToken)
-    if(AppCoToken !== null) {
+    if (AppCoToken !== null) {
       let auth = checkLoginState();
-      auth.then( headers => {
+      auth.then(headers => {
         //console.log('headers, ', headers)
-        if(typeof headers.token !== undefined) {
-          let userData = JSON.parse(sessionStorage.getItem('AppCoUser'));
-          sessionStorage.setItem('AppCoToken', headers.token);
-          this.setState({ 
+        if (typeof headers.token !== undefined) {
+          let userData = JSON.parse(sessionStorage.getItem("AppCoUser"));
+          sessionStorage.setItem("AppCoToken", headers.token);
+          this.setState({
             isLoggedIn: true,
-            userData: userData 
+            userData: userData
           });
         } else {
-          sessionStorage.removeItem('AppCoUser');
-          sessionStorage.removeItem('AppCoToken');
-          this.setState({ 
+          sessionStorage.removeItem("AppCoUser");
+          sessionStorage.removeItem("AppCoToken");
+          this.setState({
             isLoggedIn: false,
-            userData: {} 
+            userData: {}
           });
         }
       });
     }
-  }
+  };
 
-  response = (res) => {
-    if(typeof res.userData !== 'undefined') {
-      sessionStorage.setItem('AppCoUser', JSON.stringify(res.userData));
-      sessionStorage.setItem('AppCoToken', res.token);
+  response = res => {
+    if (typeof res.userData !== "undefined") {
+      sessionStorage.setItem("AppCoUser", JSON.stringify(res.userData));
+      sessionStorage.setItem("AppCoToken", res.token);
       this.setState({
-          token: res.token,
-          userNotify: res.userNotify,
-          userData: res.userData,
-          isLoggedIn: true
+        token: res.token,
+        userNotify: res.userNotify,
+        userData: res.userData,
+        isLoggedIn: true
       });
     }
-    if(typeof res.error !== 'undefined') {
-      console.error('submit error: ', res.error);
+    if (typeof res.error !== "undefined") {
+      console.error("submit error: ", res.error);
     }
-  }
+  };
 
   setStripeKey = () => {
-    if(process.env.NODE_ENV === 'production') {
-      this.stripeKey = process.env.STRIPE_PUB_KEY
+    if (process.env.NODE_ENV === "production") {
+      this.stripeKey = process.env.STRIPE_PUB_KEY;
     } else {
-      this.stripeKey = process.env.STRIPE_TEST_KEY
+      this.stripeKey = process.env.STRIPE_TEST_KEY;
     }
-  }
+  };
 
   render() {
-
     return (
       <div id="container">
-      <div id="logoBox"><img src={SetUrl() + '/AppreciateLogo.png'} alt="Appreciate Logo" /></div>
+        <div id="logoBox">
+          <img src={SetUrl() + "/AppreciateLogo.png"} alt="Appreciate Logo" />
+        </div>
         <div>
           {this.state.isLoggedIn ? (
-          <EB comp="Home">
-          <StripeProvider apiKey={this.stripeKey}>
-              <Home  />
-          </StripeProvider>
-          </EB>
+            <EB comp="Home">
+              <StripeProvider apiKey={this.stripeKey}>
+                <Home />
+              </StripeProvider>
+            </EB>
           ) : (
-              <div id="sign-in">
-                <Form formTitle="Sign In" 
+            <div id="sign-in">
+              {/* prettier-ignore */}
+              <Form formTitle="Sign In" 
                   action={`${SetUrl()}/login`}
                   valrules={ValRules} response={this.response} >
                   <Input name="email" label="Email" className="textinput" labelClass="label" errorClass="input-error"/>
@@ -99,13 +101,12 @@ class AppreciateCo extends React.Component {
                   </div>
 
                 </Form>
-              </div>
-            )}
+            </div>
+          )}
         </div>
       </div>
-    )
+    );
   }
-
 }
 
 export default AppreciateCo;
